7523c172b78faff35aed1ef18b56b3ac015ab258
Joe Bocock <joebocock@gmail.com>
2020-08-29 16:49:13 +0100

    Basic Machine tests.

--- tests/Feature/MachineTest.php
+++ tests/Feature/MachineTest.php
@@ -3,26 +3,59 @@
 namespace Tests\Feature;
 
 use App\Machine;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class MachineTest extends TestCase
 {
-    use RefreshDatabase;
-
     /**
-     * A manchine can be created.
+     * A manchine can be retrieved.
      *
      * @test
      * @return void
      */
-    public function it_can_be_created(): void
+    public function it_can_be_retrieved(): void
     {
-        Machine::create(['name' => 'Chocolate Station']);
+        $response = $this->get('/api/machines');
 
-        $this->assertDatabaseHas('machines', [
-            'name' => 'Chocolate Station',
-            'status' => 0,
-        ]);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A manchine can be stored.
+     *
+     * @test
+     * @return void
+     */
+    public function it_can_be_stored(): void
+    {
+        $response = $this->post('/api/machines', factory(Machine::class)->raw());
+
+        $response->assertStatus(201);
+    }
+
+    /**
+     * A manchine can be updated.
+     *
+     * @test
+     * @return void
+     */
+    public function it_can_be_updated(): void
+    {
+        $response = $this->put('/api/machines/' . rand(1, 10), factory(Machine::class)->raw());
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A manchine can be destroyed.
+     *
+     * @test
+     * @return void
+     */
+    public function it_can_be_destroyed(): void
+    {
+        $response = $this->delete('/api/machines/' . rand(1, 10));
+
+        $response->assertStatus(204);
     }
 }
