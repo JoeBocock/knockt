04feb13c9b87c4c9fbbefdafc6c4172190e520c0
Joe Bocock <joebocock@gmail.com>
2020-08-29 16:09:53 +0100

    Let us seed, dear Watson.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call([
+            MachineSeeder::class,
+            RowSeeder::class,
+            ProductSeeder::class,
+            SlotSeeder::class,
+        ]);
     }
 }
