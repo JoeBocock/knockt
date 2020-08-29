db6902e5b680dc0864076bcad170c57a41de0e51
Joe Bocock <joebocock@gmail.com>
2020-08-29 16:48:48 +0100

    Clean as not writing full Unit tests yet.

---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class DeletionTest extends TestCase
+{
+    /**
+     * Test the cascade of deletion for a machine.
+     *
+     * @test
+     * @return void
+     */
+    public function it_will_assert_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
