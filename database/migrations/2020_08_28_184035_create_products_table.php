99af9632ea60c23a7f4515433a545cc25a6349ea
Joe Bocock <joebocock@gmail.com>
2020-09-26 12:45:36 +0100

    Modify table structure for a product.

--- database/migrations/2020_08_28_184035_create_products_table.php
+++ database/migrations/2020_08_28_184035_create_products_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateProductsTable extends Migration
 {
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedInteger('price');
             $table->bigInteger('stock')->default(0);
             $table->timestamps();
         });
