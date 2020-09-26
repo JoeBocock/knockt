cb3c9e8a2e73495ac04cb49995c02c8aa5e36952
Joe Bocock <joebocock@gmail.com>
2020-09-26 12:46:10 +0100

    Update factory.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,6 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->word(),
-        'stock' => $faker->numberBetween(1, 99999)
+        'price' => $faker->numberBetween(1, 99999),
+        'stock' => $faker->numberBetween(1, 99999),
     ];
 });
