4c387f87375813f6481cc7098b80d4636b612f4c
Joe Bocock <joebocock@gmail.com>
2021-03-31 16:02:53 +0100

    Basic Migrations, Controllers and Models.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\MachineController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RowController;
+use App\Http\Controllers\SlotController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,68 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/*
+|--------------------------------------------------------------------------
+| Machine Routes
+|--------------------------------------------------------------------------
+|
+| Used to manage the 'Machine' resource.
+|
+| [GET] /api/machines          Lists all Machines
+| [POST] /api/machines         Store a new machine
+| [GET] /api/machines/{id}     Retrieve a specific machine
+| [PUT] /api/machines/{id}     Update an existing machine
+| [DELETE] /api/machines/{id}  Remove an existing machine
+|
+*/
+Route::apiResource('machines', MachineController::class);
+
+/*
+|--------------------------------------------------------------------------
+| Row Routes
+|--------------------------------------------------------------------------
+|
+| Used to manage the 'row' resource.
+|
+| [GET] /api/rows          Lists all Rows
+| [POST] /api/rows         Store a new row
+| [GET] /api/rows/{id}     Retrieve a specific row
+| [PUT] /api/rows/{id}     Update an existing row
+| [DELETE] /api/rows/{id}  Remove an existing row
+|
+*/
+Route::apiResource('rows', RowController::class);
+
+/*
+|--------------------------------------------------------------------------
+| Slot Routes
+|--------------------------------------------------------------------------
+|
+| Used to manage the 'slot' resource.
+|
+| [GET] /api/slots          Lists all of this resource type
+| [POST] /api/slots         Store a new slot
+| [GET] /api/slots/{id}     Retrieve a specific slot
+| [PUT] /api/slots/{id}     Update an existing slot
+| [DELETE] /api/slots/{id}  Remove an existing slot
+|
+| [POST] /api/slots/{slot}/purchase Purchase the product within a slot.
+|
+*/
+Route::apiResource('slots', SlotController::class);
+
+/*
+|--------------------------------------------------------------------------
+| Product Routes
+|--------------------------------------------------------------------------
+|
+| Used to manage the 'product' resource.
+|
+| [GET] /api/products          Lists all of this resource type
+| [POST] /api/products         Store a new product
+| [GET] /api/products/{id}     Retrieve a specific product
+| [PUT] /api/products/{id}     Update an existing product
+| [DELETE] /api/products/{id}  Remove an existing product
+|
+*/
+Route::apiResource('products', ProductController::class);
