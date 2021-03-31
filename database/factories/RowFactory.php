7a5b3eb9327521800cc05165f6c346d599839f00
Joe Bocock <joebocock@gmail.com>
2021-03-31 21:34:44 +0100

    Factories

---
+++ database/factories/RowFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Row;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RowFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Row::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'machine_id' => rand(1, 10),
+        ];
+    }
+}
---
+++ database/factories/SlotFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Slot;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SlotFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Slot::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->bothify('#?'),
+            'row_id' => rand(1, 30),
+            'product_id' => rand(1, 100),
+        ];
+    }
+}
