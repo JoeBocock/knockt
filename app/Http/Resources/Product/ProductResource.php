0088de0962f366d75f39ab60d23074c2e47ba070
Joe Bocock <joebocock@gmail.com>
2021-03-31 21:34:21 +0100

    Resources

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
---
+++ app/Http/Resources/Slot/SlotResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources\Slot;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SlotResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
